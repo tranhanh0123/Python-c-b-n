fb66bae917382e2ed14939967951ddf675011a27
tranhanh0123 <tranvanhanh030595@gmail.com>
2021-09-02 18:29:02 +0700

    xinchao

--- Thao-tac-voi-file-txt.py
+++ Thao-tac-voi-file-txt.py
@@ -1,6 +1,21 @@
 import os
 import io
 
+# lấy ra đường dẫn hiện tại đang code
+# a = os.getcwd()
+# print(a)
 
-a = os.getcwd()
-print(a)
+
+#chuyển sang một đường dẫn khác
+# a = os.chdir(r"C:\Users\Admin\Desktop\New folder")
+# print(a)
+
+
+#mở một file. nếu chưa có file sẽ tự tạo
+# def mo_file():
+#     f = open("test.txt","w")
+#     f.close()
+# mo_file()
+
+
+
