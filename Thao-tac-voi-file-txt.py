db234f4a1588cac4f1375790fbde2e274c7bf538
tranhanh0123 <tranvanhanh030595@gmail.com>
2021-09-03 16:50:11 +0700

    xinchao

--- Thao-tac-voi-file-txt.py
+++ Thao-tac-voi-file-txt.py
@@ -7,7 +7,7 @@ import io
 
 
 #chuyển sang một đường dẫn khác
-# a = os.chdir(r"C:\Users\Admin\Desktop\New folder")
+# a = os.chdir(r"E:\Laptrinhpython\Thao-tac-voi-file-trong-python")
 # print(a)
 
 
@@ -17,6 +17,60 @@ import io
 #     f.close()
 # tenfile="vidu.txt"
 # mo_file(tenfile)
+
+
+#đổi tên một file
+# def doi_ten_file():
+#     f = os.rename("vidu.txt","Testdoiten.txt")
+# doi_ten_file()
+
+
+
+#xóa một file
+# def xoa_file():
+#     f = os.remove("Testdoiten.txt")
+# xoa_file()
+
+
+
+#coppy một file
+# from shutil import copyfile
+# def coppy_file():
+#     f =copyfile("vidu.txt","vidu222.txt")
+# coppy_file()
+
+
+
+#ghi file ko utf8
+# def ghifile(tenfile,noidung):
+#     f = open(tenfile,"a")
+#     f.write(noidung+'\n')
+#     f.close()
+# tenfile = "vidu.txt"
+# noidung = "con cac"
+# ghifile(tenfile,noidung)
+
+
+
+#ghi file có tiếng việt (UTF8)
+# def ghi_file(filename,nd_line):
+#     f = io.open(filename, "a", encoding="utf-8")
+#     f.write(nd_line + '\n')
+#     f.close()
+# filename = "test.txt"
+# nd_line = " xin chào cả nhà nhé @@"
+# ghi_file(filename,nd_line)
+
+
+
+#đọc file có utf8
+# def doc_file():
+#     f = io.open("test.txt", mode="r", encoding="utf-8")
+#     doc_van_ban = f.readlines()
+    # print(doc_van_ban)
+#     for i in doc_van_ban:
+#         print(i)
+# doc_file()
 
 
 
