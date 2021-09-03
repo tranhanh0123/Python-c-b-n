bb04aad32a20b37382b80f13e7fb17ecbc8250cf
tranhanh0123 <tranvanhanh030595@gmail.com>
2021-09-03 18:03:15 +0700

    xinchao

---
+++ Thao-tac-voi-file-excel.py
@@ -0,0 +1,37 @@
+import openpyxl
+
+
+
+
+#đọc ô bất kì trong excel :
+# def get_value_excel(filename, cellname):
+#     wb = openpyxl.load_workbook(filename)
+#     Sheet1 = wb['Sheet1']
+#     wb.close()
+#     return Sheet1[cellname].value
+
+# filename = "test.xlsx"
+# cellname = "D5"
+# print(get_value_excel(filename, cellname))
+
+
+
+#update một ô mới
+# def update_value_excel(filename, cellname, value):
+#     wb = openpyxl.load_workbook(filename)
+#     Sheet1 = wb['Sheet1']
+#     Sheet1[cellname].value = value
+#     wb.close()
+#     wb.save(filename)
+
+# filename = "test.xlsx"
+# cellname = "D5"
+# value = "concac123"
+# update_value_excel(filename, cellname, value)
+
+
+
+
+
+
+
